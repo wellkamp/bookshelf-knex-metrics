811e7a2492c068d5c41363e0451e5b88a81539b1
wellkamp <wellkamp@gmail.com>
2021-05-10 15:37:04 -0300

    fix configuração db

--- api/config/db.js
+++ api/config/db.js
@@ -1,10 +1,10 @@
 const knex = require("knex")({
-  client: "mysql",
+  client: "mysql2",
   connection: {
     host: "127.0.0.1",
-    user: "your_database_user",
-    password: "your_database_password",
-    database: "myapp_test",
+    user: "root",
+    password: "pass",
+    database: "bookshelf_metrics",
     charset: "utf8",
   },
 });
